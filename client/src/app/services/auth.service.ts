eba26277b3a1212bc3fd24fead8a38aac5a8af04
gugui3z24 <gugui3z24@gmail.com>
2017-07-11 22:14:41 -0400

    api update

--- client/src/app/services/auth.service.ts
+++ client/src/app/services/auth.service.ts
@@ -6,7 +6,7 @@ import { tokenNotExpired } from 'angular2-jwt';
 @Injectable()
 export class AuthService {
 
-  domain = "http://localhost:8080/"; // Development Domain - Not Needed in Production
+  domain = ""; // Development Domain - Not Needed in Production
   authToken;
   user;
   options;
