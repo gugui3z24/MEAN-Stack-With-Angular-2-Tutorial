62d70b70f3e2f0bba58f0ab43f21e68ccfc0a4c0
gbakalov <georgi.bakalov.is@gmail.com>
2017-12-14 23:31:58 +0200

    Fix for MongoDB v3.2> "Unknown modifier: $pushAll" error retrieved.

--- models/user.js
+++ models/user.js
@@ -134,6 +134,8 @@ const userSchema = new Schema({
   email: { type: String, required: true, unique: true, lowercase: true, validate: emailValidators },
   username: { type: String, required: true, unique: true, lowercase: true, validate: usernameValidators },
   password: { type: String, required: true, validate: passwordValidators }
+}, {
+  usePushEach: true
 });
 
 // Schema Middleware to Encrypt Password
